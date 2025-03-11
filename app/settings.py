b52dc04a015bdb595549e38c303c0752a3060c6e
Auxiliar Bi <marcus_nascimento@live.com>
2025-03-11 16:37:46 -0300

    insert config database

--- app/settings.py
+++ app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tickets',
+        'USER': 'postgres',
+        'PASSWORD': 'twdpsy4i20',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
